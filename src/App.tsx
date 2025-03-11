2340645d1d7aca3ee9ff5441dce6ae92ce948f2d
MauriRiver345 <mrg2735@gmail.com>
2025-03-11 10:56:09 -0600

    feat: :sparkles: La carta para cada cafe es funcional

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import CofeeCard from "./components/cofeeCard"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CofeeCard coffeeName='Capuchino' price={5.2} reviews={3.4} votes={140} disponible={true} url = "https://th.bing.com/th/id/R.618d65b48109afab15e8c63ead59f78b?rik=U121Gcu9DBouow&riu=http%3a%2f%2fokoffe.ru%2fwp-content%2fuploads%2f2018%2f06%2fNapitok-Latte.jpg&ehk=VKMwVmTRgefRc39Qmbvy%2bSvEosoC%2fCBk24bQp%2bBvWJw%3d&risl=&pid=ImgRaw&r=0"></CofeeCard>
     </>
   )
 }
