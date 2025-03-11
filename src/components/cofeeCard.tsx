1e7b5f568894a7c52239ce08b17432ee809d1c84
MauriRiver345 <mrg2735@gmail.com>
2025-03-11 12:10:46 -0600

    feat: :sparkles: Ahora se muestra si el café esta o no disponible

--- src/components/cofeeCard.tsx
+++ src/components/cofeeCard.tsx
@@ -2,17 +2,18 @@ import './cofeeCard.css'
 import { Coffee } from '../models/Coffee'
 
 export function CoffeeCard ({
-  available,
+  popular,
   image,
   name,
   price,
   rating,
-  votes
+  votes,
+  available
 }: Coffee) {
   return (
     <div className="cofee-card">
       <div className="imgContainer" style={{ backgroundImage: `url(${image})` }}>
-        {available && <p className='isPopular'>Popular</p>}
+        {popular && <p className='isPopular'>Popular</p>}
       </div>
       <div className="data">
         <div className="product">
@@ -21,7 +22,10 @@ export function CoffeeCard ({
             <p style={{ color: "black" }}>{price}</p>
           </div>
         </div>
+        <div className="details">
         <p id='reviews'>⭐{rating}<span id='votes'>({votes} votes)</span></p>
+        <p id='availability'>{available ? '' : 'Sold out'}</p>
+        </div>
       </div>
     </div>
   )
